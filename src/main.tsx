21861cee383735d209b1c91136ccdf53f98bc111
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2023-03-16 00:50:20 +0700

    feat:redux-toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@emotion/react";
+import { Provider } from "react-redux";
 import { CssBaseline } from "@mui/material";
 import * as ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
@@ -6,12 +7,17 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/root";
 import theme from "./theme";
 
+//store
+import { store } from "./redux-toolkit/store";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  // <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <RouterProvider router={router} />
-    <Toaster />
-  </ThemeProvider>
-  // </React.StrictMode>,
+  <Provider store={store}>
+    {/* <React.StrictMode> */}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+      <Toaster />
+    </ThemeProvider>
+    {/* </React.StrictMode>, */}
+  </Provider>
 );
---
+++ src/redux-toolkit/auth/auth-slice.ts
@@ -0,0 +1,23 @@
+import { RootState } from "../store";
+import { createSlice } from "@reduxjs/toolkit";
+import { Account } from "../../app-types/account.type";
+
+type AuthState = {
+  isAuthLoading: boolean;
+  account: Account | null;
+};
+
+const initialState: AuthState = {
+  isAuthLoading: true,
+  account: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {},
+});
+
+export const selectAuthState = (state: RootState) => state.authState;  //AuthState ถ้าอยากใช้ isAuthLoading , account ให้ใช้ selectAuthState
+
+export default authSlice.reducer;
